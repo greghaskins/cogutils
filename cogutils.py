987927c1b6f40de70f028208b7a3449ec1668d0b
Greg Haskins <greg@greghaskins.com>
2011-06-12 13:44:23 -0400

    Minor refactor, version bump

--- cogutils.py
+++ cogutils.py
@@ -2,40 +2,32 @@
 
 Requires: 
 cog: http://pypi.python.org/pypi/cogapp
+Optional:
 pygments: http://pygments.org/
 """
 
-import pygments
-import pygments.lexers
-import pygments.formatters
-
-def set_syntax_formatter(formatter=None):
-    """Sets or determines the appropriate syntax formatter for this file."""
+def include(filename, start=None, end=None):
+    """Include the text of an external file verbatim."""
     import cog
-    if formatter is None:
-        formatter = pygments.formatters.get_formatter_for_filename(cog.outFile)
-    elif isinstance(formatter, basestring):
-        formatter = pygments.formatters.get_formatter_by_name(formatter)
-    cog.pygments_formatter = formatter
-    return formatter
-
-def get_syntax_formatter():
-    """Returns the pygments syntax formatter, if one is set."""
-    import cog
-    try:
-        return cog.pygments_formatter
-    except AttributeError:
-        raise RuntimeError('no formatter set. call set_syntax_formatter() first')
+    with open(filename, 'r') as f:
+        text = f.read()
+    text = _subset_lines(text, start, end)
+    cog.outl(text)
     
-def print_syntax_styles():
-    """Print the style definitions for the formatter. Note that this does NOT
-    include the wrapping <style> tag."""
-    import cog
-    formatter = get_syntax_formatter()
-    cog.out(formatter.get_style_defs())
-    
+def Includer(prefix="", suffix=""):
+    """Wraps the include function by prepending and appending fixed text to
+    its output."""
+    def _include(*args, **kwargs):
+        import cog
+        cog.out(prefix)
+        include(*args, **kwargs)
+        cog.out(suffix)
+    return _include
+
 def include_with_syntax(filename, start=None, end=None, lexer=None):
     """Includes the text of an external file, highlighting with pygments."""
+    import pygments
+    import pygments.lexers
     import cog
     
     with open(filename, 'r') as f:
@@ -51,15 +43,36 @@ def include_with_syntax(filename, start=None, end=None, lexer=None):
     
     cog.out(pygments.highlight(text, lexer, formatter))
 
-def include(filename, start=None, end=None):
-    """Include the text of an external file verbatim."""
+def set_syntax_formatter(formatter=None):
+    """Sets or determines the appropriate syntax formatter for this file."""
+    import pygments.formatters
     import cog
-    with open(filename, 'r') as f:
-        text = f.read()
-    text = _subset_lines(text, start, end)
-    cog.out(text)
+    if formatter is None:
+        formatter = pygments.formatters.get_formatter_for_filename(cog.outFile)
+    elif isinstance(formatter, basestring):
+        formatter = pygments.formatters.get_formatter_by_name(formatter)
+    cog.pygments_formatter = formatter
+    return formatter
+
+def get_syntax_formatter():
+    """Returns the pygments syntax formatter, if one is set."""
+    import cog
+    try:
+        return cog.pygments_formatter
+    except AttributeError:
+        raise RuntimeError("no formatter set. call set_syntax_formatter() "
+                           "first")
+    
+def print_syntax_styles():
+    """Print the style definitions for the formatter. Note that this does NOT
+    include the wrapping <style> tag."""
+    import cog
+    formatter = get_syntax_formatter()
+    cog.out(formatter.get_style_defs())
     
 def _subset_lines(text, start=None, end=None):
+    """Utility function returning only lines between the given start and end
+    line numbers (inclusive)."""
     if start or end:
         lines = text.split('\n')
         start = 1 if start is None else start
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='Cogutils',
-      version='0.11',
+      version='0.15',
       description='Utilities for text processing with Cog',
       author='Greg Haskins',
       author_email='greg@greghaskins.com',
-      url='http://blog.greghaskins.com/projects/cogutils/',
+      url='https://github.com/greghaskins/cogutils/',
       py_modules=['cogutils'],
      )
