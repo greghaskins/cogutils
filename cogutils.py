b2aed7eab6df2edfb00cecb438ca1d215ac892f0
Greg Haskins <greg@greghaskins.com>
2011-06-12 12:53:58 -0400

    Initial commit

---
+++ cogutils.py
@@ -0,0 +1,69 @@
+"""Utility functions for use in cog scripts. 
+
+Requires: 
+cog: http://pypi.python.org/pypi/cogapp
+pygments: http://pygments.org/
+"""
+
+import pygments
+import pygments.lexers
+import pygments.formatters
+
+def set_syntax_formatter(formatter=None):
+    """Sets or determines the appropriate syntax formatter for this file."""
+    import cog
+    if formatter is None:
+        formatter = pygments.formatters.get_formatter_for_filename(cog.outFile)
+    elif isinstance(formatter, basestring):
+        formatter = pygments.formatters.get_formatter_by_name(formatter)
+    cog.pygments_formatter = formatter
+    return formatter
+
+def get_syntax_formatter():
+    """Returns the pygments syntax formatter, if one is set."""
+    import cog
+    try:
+        return cog.pygments_formatter
+    except AttributeError:
+        raise RuntimeError('no formatter set. call set_syntax_formatter() first')
+    
+def print_syntax_styles():
+    """Print the style definitions for the formatter. Note that this does NOT
+    include the wrapping <style> tag."""
+    import cog
+    formatter = get_syntax_formatter()
+    cog.out(formatter.get_style_defs())
+    
+def include_with_syntax(filename, start=None, end=None, lexer=None):
+    """Includes the text of an external file, highlighting with pygments."""
+    import cog
+    
+    with open(filename, 'r') as f:
+        text = f.read()
+        
+    if lexer is None:
+        lexer = pygments.lexers.guess_lexer_for_filename(filename, text)
+    elif isinstance(lexer, basestring):
+        lexer = pygments.lexers.get_lexer_by_name(lexer)
+
+    formatter = get_syntax_formatter()
+    text = _subset_lines(text, start, end)
+    
+    cog.out(pygments.highlight(text, lexer, formatter))
+
+def include(filename, start=None, end=None):
+    """Include the text of an external file verbatim."""
+    import cog
+    with open(filename, 'r') as f:
+        text = f.read()
+    text = _subset_lines(text, start, end)
+    cog.out(text)
+    
+def _subset_lines(text, start=None, end=None):
+    if start or end:
+        lines = text.split('\n')
+        start = 1 if start is None else start
+        end = len(lines) + 1 if end is None else end
+        text = '\n'.join(lines[start-1:end])
+    return text
+        
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Cogutils',
+      version='0.11',
+      description='Utilities for text processing with Cog',
+      author='Greg Haskins',
+      author_email='greg@greghaskins.com',
+      url='http://blog.greghaskins.com/projects/cogutils/',
+      py_modules=['cogutils'],
+     )
